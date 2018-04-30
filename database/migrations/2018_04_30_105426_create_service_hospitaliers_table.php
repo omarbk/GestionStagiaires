788060ad65fa4a2fb49a04f0199725333e694aad
omar <omar@MacBook-Pro-de-omar.local>
2018-04-30 12:02:01 +0100

    lund3/30/2018

--- database/migrations/2018_04_30_105426_create_service_hospitaliers_table.php
+++ database/migrations/2018_04_30_105426_create_service_hospitaliers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStagesGroupesTable extends Migration
+class CreateServiceHospitaliersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateStagesGroupesTable extends Migration
      */
     public function up()
     {
-        Schema::create('stage_groupes', function (Blueprint $table) {
+        Schema::create('service_hospitaliers', function (Blueprint $table) {
+            
             $table->increments('id_stage_groupe');
             $table->integer('fk_stage');
             $table->integer('fk_groupe');
             $table->softDeletes();
-            $table->timestamps();
+
         });
     }
 
@@ -29,6 +30,6 @@ class CreateStagesGroupesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('stage_groupes');
+        Schema::dropIfExists('service_hospitaliers');
     }
 }
