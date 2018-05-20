a787bc1ff3f8f3b4091a11942b510adc2a8a0393
omar <omar@MBP-de-omar.Home>
2018-05-20 20:39:13 +0000

    dim/20/2018

--- app/Http/Controllers/StagiaireController.php
+++ app/Http/Controllers/StagiaireController.php
@@ -7,13 +7,14 @@ use Auth;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
-
+use App\Type_objectif; 
 use App\Stagiaire;
 use App\Evaluation;
 use App\Stage;
 use App\User;
 use App\Evaluateur;
-
+use App\Evaluation_objectif;
+use PDF;
 class StagiaireController extends Controller
 {
     public function countUser(){
@@ -160,7 +161,6 @@ class StagiaireController extends Controller
                 
                             ->select('stagiaires.*', 'users.*','stages.*','groupes.*')
                             ->where('stagiaires.id_stagiaire','=',$id_stagiaire)
-                            
                             ->get();
                            // dd($stagiaire);
 
@@ -298,6 +298,169 @@ public function getStagiairesParEvaluation(Request $request){
     ->get();
     
 return Response()->json(['stagiaires' => $stagiaires]);
+
+}
+public function PDFevaluation($s,$st){
+    $stagiaires  = Stagiaire::leftJoin('groupe_stagiaires','stagiaires.id_stagiaire','=','groupe_stagiaires.fk_stagiaire')
+    ->leftJoin('groupes', 'groupe_stagiaires.fk_groupe', '=', 'groupes.id_groupe')
+    ->leftJoin('stage_groupes', 'groupes.id_groupe', '=', 'stage_groupes.fk_groupe')
+    ->leftJoin('stages', 'stage_groupes.fk_stage', '=', 'stages.id_stage')
+    ->leftJoin('hospitaliers', 'stages.fk_hospitalier', '=', 'hospitaliers.id_hospitalier')
+    ->leftJoin('evaluateurs', 'stages.fk_evaluateur', '=', 'evaluateurs.id_evaluateur')
+    ->select('stagiaires.*','groupes.*','stages.*','hospitaliers.*','evaluateurs.*')
+    ->where('stagiaires.id_stagiaire','=',$s)
+    ->where('stages.id_stage','=',$st)
+    ->get();
+    $objectifs = Evaluation_objectif::leftJoin('type_objectifs', 'evaluation_objectifs.fk_type_objectif', '=', 'type_objectifs.id_type')
+    ->leftJoin('notes', 'evaluation_objectifs.id_evaluation_objectif', '=', 'notes.fk_objectif')
+    ->leftJoin('evaluations', 'evaluations.id_evaluation', '=', 'notes.fk_evaluation')
+    ->leftJoin('total_notes', 'evaluations.id_evaluation', '=', 'total_notes.fk_evaluation')
+    ->leftJoin('stagiaires', 'evaluations.fk_stagiaire', '=', 'stagiaires.id_stagiaire')
+    ->leftJoin('stages', 'evaluations.fk_stage', '=', 'stages.id_stage')
+
+    ->select('evaluation_objectifs.*', 'type_objectifs.*','notes.*','evaluations.*','total_notes.*','stages.*')
+    ->where('stagiaires.id_stagiaire','=',$s)
+    ->where('stages.id_stage','=',$st)
+    ->get();
+    //dd($stagiaires);
+
+    $typeObjectifs = Type_objectif::leftJoin('note_types', 'type_objectifs.id_type', '=', 'note_types.fk_type_objectif')
+    ->leftJoin('evaluations', 'evaluations.id_evaluation', '=', 'note_types.fk_evaluation')
+    ->leftJoin('stages', 'evaluations.fk_stage', '=', 'stages.id_stage')
+
+    ->select( 'type_objectifs.*','evaluations.*','note_types.*','stages.*')
+    ->where('evaluations.fk_stagiaire','=',$s)
+    ->where('stages.id_stage','=',$st)
+
+    ->get();
+
+    
+//return Response()->json(['stagiaires' => $stagiaires,'objectifs' =>$objectifs,'typeObjectifs',$typeObjectifs ]);
+  // dd($stagiaires);
+
+   // 
+   PDF::setFooterCallback(function($pdf) {
+    //dd($infoComp);
+   // dd($this->template);
+     // Position at 15 mm from bottom
+    // $pdf->SetY(-15);
+     // Set font
+     $pdf->SetFont('helvetica', 'I', 10);
+     // Page number
+     PDF::writeHTMLCell(0, 0, '',250,'Cachet et signature du chef de service', 0, 1, 0, true, '', true);
+     PDF::writeHTMLCell(0, 0, 130,250,'Cachet et signature des maitres de stage', 0, 1, 0, true, '', true);
+     PDF::writeHTMLCell(0, 0,'',285,'<hr>', 0, 1, 0, true, '', true);
+     PDF::writeHTMLCell(0, 0, '',290,'<span style="color:blue;text-align:right"> Page '.$pdf->getAliasNumPage().'/'.$pdf->getAliasNbPages().'</span>', 0, 1, 0, true, '', true);
+
+    // $pdf->Cell(0, 10, $this->template.'Page '.$pdf->getAliasNumPage().'/'.$pdf->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+
+ });
+    PDF::SetTitle('Evaluation');
+
+    PDF::AddPage();
+
+    $headTop = '<h2 style="color:red"> Fiche d évaluation du stage </h2>';
+    $numStage = '<h2 style="color:red">'.$stagiaires[0]->intitule_stage.'</h2>';
+
+
+   
+
+    $commandesHtml ='<table  style="padding: 3px 0px;" cellpadding="2">
+    <thead>
+    <tr>
+           <th></th>
+           <th></th>
+           <th></th>
+           <th></th>
+    </tr>
+            </thead>';
+     $commandesHtml.='<tbody>';
+    foreach ( $typeObjectifs as $typeObjectif ){
+        $commandesHtml.='
+       
+        <tr>                   
+                <th align="center" style="width:450px;color:blue"><h3>'.$typeObjectif->type_objectif.'</h3></th>
+                
+         </tr>
+         ';
+        foreach ( $objectifs as $objectif){ 
+            if($objectif->fk_type_objectif===$typeObjectif->id_type){
+        $commandesHtml.='
+             <tr>             
+                <th style="
+                border-left:1px solid black;
+                border-bottom: 1px solid black;
+                border-top: 1px solid black;width:400px;
+                ">'.$objectif->objectif.'</th>
+                <th align="center" style="
+                border-right:1px solid black;
+                border-bottom: 1px solid black;
+                border-top: 1px solid black;
+                border-left:1px solid black;
+                ">'.$objectif->note.'/'.$objectif->coefficient.'</th>
+                
+                
+         </tr> '; }
+        }
+    
+    
+        $commandesHtml.='
+       
+        <tr>    
+        <th style="
+        border-left:1px solid black;
+        border-bottom: 1px solid black;
+        border-top: 1px solid black;width:400px;
+        ">Note </th>
+        <th align="center" style="
+        border-right:1px solid black;
+        border-bottom: 1px solid black;
+        border-top: 1px solid black;
+        border-left:1px solid black;
+        ">'.$typeObjectif->note_type.'/20</th>
+
+                
+         </tr>
+         ';}
+        $commandesHtml.='</tbody> </table>';
+        
+        $valide= '<table>
+       
+            <tr>
+            <td>Score totale sur 60 :</td>
+            <td>Note finale '.$objectifs[0]->noteFinal.' /20</td>';
+            if($objectifs[0]->etat === "non valide"){
+                $valide.='<td style="color:red">non valide</td>'; }
+             else {
+                    $valide.='<td style="color:green">valide</td>'; }    
+                    $valide.= '</tr>
+        
+        </table>';
+
+        PDF::writeHTMLCell(0, 0,63, 4,$headTop,0, 1, 0, true, '', true);    
+        PDF::writeHTMLCell(0, 0,88, 11,$numStage,0, 1, 0, true, '', true); 
+        PDF::writeHTMLCell(0, 0,'', 24,'<hr>',0, 1, 0, true, '', true); 
+        PDF::writeHTMLCell(0, 0,'', 30,'Année Universitaire :'.$stagiaires[0]->annee_universitaire_stagiaire.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,130, 30,''.$stagiaires[0]->niveau_etude_stagiaire.' de medecine',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,'', 40,'Nom : '.$stagiaires[0]->nom_stagiaire.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,70, 40,'Prénom : '.$stagiaires[0]->prenom_stagiaire.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,140, 40,'N° : '.$stagiaires[0]->id_stagiaire.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,'', 50,'Période du stage du : ',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,70, 50,$stagiaires[0]->dateDebut_stage,0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,120, 50,'au : ',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,150, 50,$stagiaires[0]->dateFin_stage,0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,'', 60,'Hospital : '.$stagiaires[0]->nom_hospitalier.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,'', 70,'Service : '.$stagiaires[0]->service_evaluateur.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,80, 70,'Nom du MS : '.$stagiaires[0]->service_evaluateur.'',0, 1, 0, true, '', true);
+        PDF::writeHTMLCell(0, 0,'', 79,'<hr>',0, 1, 0, true, '', true); 
+        PDF::writeHTMLCell(0, 0,'', 82,$commandesHtml,0, 1, 0, true, '', true);   
+        $y = PDF::getY()+8; 
+        PDF::writeHTMLCell(0, 0,73, $y,'<h3 style="color:blue">Examin de fin de stage</h3>',0, 1, 0, true, '', true); 
+        PDF::writeHTMLCell(0, 0,130, $y+0.5,'note : '.$objectifs[0]->noteStage.' / 20',0, 1, 0, true, '', true); 
+        $y = PDF::getY()+8; 
+        PDF::writeHTMLCell(0, 0,'', $y,$valide,1, 1, 0, true, '', true);
+        
+    PDF::Output('hello_world.pdf');
 
 }
 
